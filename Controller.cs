b769c2fd2d7d33f775370f7098943afe8b693bdd
zenaphobia <alexkmf96@gmail.com>
2022-10-25 21:14:24 -0700

    edited controller.cs to act as a whole controller for the UX

--- Controller.cs
+++ Controller.cs
@@ -8,5 +8,21 @@ namespace ZombieSimulator
         {
 
         }
+
+        public void deployChoice( string _choice, SurvivorBase _survivorBase )
+        {
+            int _typedChoice;
+            switch( _choice )
+            {
+                case "1" or "Search":
+                    _typedChoice = int.Parse(Console.ReadLine());
+                    _survivorBase.search(_typedChoice);
+                    break;
+                case "2" or "repair":
+                    _typedChoice = int.Parse(Console.ReadLine());
+                    _survivorBase.repair(_typedChoice);
+                    break;
+            }
+        }
     }
 }
