6b7d31ee62417aa5e63c5f46d96fda22e5ede5aa
zenaphobia <alexkmf96@gmail.com>
2022-10-26 20:53:07 -0700

    fixed deplyChoice to have 3 paramters in switch statement

--- Controller.cs
+++ Controller.cs
@@ -12,16 +12,20 @@ namespace ZombieSimulator
         public void deployChoice( string _choice, SurvivorBase _survivorBase )
         {
             int _typedChoice;
+            _typedChoice = int.Parse(Console.ReadLine());
             switch( _choice )
             {
                 case "1" or "Search":
-                    _typedChoice = int.Parse(Console.ReadLine());
                     _survivorBase.search(_typedChoice);
                     break;
                 case "2" or "repair":
-                    _typedChoice = int.Parse(Console.ReadLine());
                     _survivorBase.repair(_typedChoice);
                     break;
+                case "3":
+                    int _intHours = int.Parse(Console.ReadLine());
+                    _survivorBase.searchForSurvivors( _intHours );
+                    break;
+
             }
         }
     }
